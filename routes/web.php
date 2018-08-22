02c6e12a6124270c4c572d7840074de9daab094d
Brandon Bradlee <brandon.bradlee@gmail.com>
2018-08-22 16:10:46 -0400

    Home

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,17 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
